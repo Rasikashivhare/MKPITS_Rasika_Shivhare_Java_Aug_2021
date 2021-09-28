aa9bbaafc8baa1d455a5fad8fdeafc6620e85f0b
Rasikashivhare <rasikashivhare123@gmail.com>
2021-09-28 21:28:50 +0530

    28sept

---
+++ Java_Programs/javaexamples folder/Dog1.java
@@ -0,0 +1,14 @@
+class Animal
+{
+void eat()
+{System.out.println("animal is eating ..");}
+}
+class Dog1 extends Animal{
+void eat(){System.out.println("dog is eating...");}
+public static void main(String []arg)
+{
+Animal a=new Dog1
+();
+a.eat();
+}
+}
