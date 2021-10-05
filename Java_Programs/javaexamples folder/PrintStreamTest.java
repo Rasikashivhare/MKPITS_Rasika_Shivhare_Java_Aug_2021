d4f11f25e28a057d9e4a28d162fc8bf9e111bd96
Rasikashivhare <rasikashivhare123@gmail.com>
2021-10-05 16:14:14 +0530

    5oct

---
+++ Java_Programs/javaexamples folder/PrintStreamTest.java
@@ -0,0 +1,16 @@
+//Example of java PrintStream class
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+public class PrintStreamTest{
+public static void main(String args[])throws Exception{
+FileOutputStream fout=new FileOutputStream("D:\\testout.txt");
+PrintStream pout=new PrintStream(fout);
+pout.println(2016);
+pout.println("Hello Java");
+pout.println("Welcome to Java");
+pout.close();
+fout.close();
+System.out.println("Success?");
+}
+}
---
+++ Java_Programs/javaexamples folder/PrintStreamTest1.java
@@ -0,0 +1,9 @@
+//Example of printf() method using java PrintStream class
+
+class PrintStreamTest1
+{
+public static void main(String args[]){
+int a=19;
+System.out.printf("%d",a); //Note: out is the object of printstream
+}
+}
