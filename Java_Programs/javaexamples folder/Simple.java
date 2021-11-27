c36c5becadc90a56d9d797e790d29c3ffa9edf39
Rasikashivhare <rasikashivhare123@gmail.com>
2021-11-27 09:36:39 +0530

    26nov

--- Java_Programs/javaexamples folder/Simple.java
+++ Java_Programs/javaexamples folder/Simple.java
@@ -1,8 +1,15 @@
-//example of subpackage 
-package com.mk.core;
-class  Simple{
-public static void main(String args[])
-{
-System.out.println("Hello subpackage");
-}
+
+import java.sql.*;
+public class simple {
+    public static void main(String[] arg){
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection
+                    con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mkpits123","root","");
+                    System.out.println("successfully connected");
+        }catch(Exception ee){
+            System.out.println(ee.toString());
+        }
+    }
 }
