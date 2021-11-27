632c20cd2d9202497c2289757bf7ab552829978e
Rasikashivhare <rasikashivhare123@gmail.com>
2021-11-27 09:37:56 +0530

    26nov

--- Java_Programs/javaexamples folder/sample.java
+++ Java_Programs/javaexamples folder/sample.java
@@ -1,24 +1,19 @@
-//example
-class sample
-{
-public static void main(String[] arg) {
-int arr[] =new int[5];
-int res=0;
-try {
-int num[]=new int[3];
 
-num[4]=33;
-
-}catch (Exception ee) {
-System.out.println(ee.toString());
-
-}
-finally {
-System.out.println("finally executed");
-}
-System.out.println("bye");
-
-System.out.println("program reached successfully till end");
-}
-
+import java.sql.*;
+public class sample {
+    public static void main(String[] arg){
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection
+                    con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mkpits123","root","");
+                    System.out.println("successfully connected");
+            Statement stmt=con.createStatement();
+            String qr="update students set course_name='html' where student_id=1 ";
+            stmt.executeUpdate(qr);
+            System.out.println("record updated successfully");
+        }catch(Exception ee){
+            System.out.println(ee.toString());
+        }
+    }
 }
