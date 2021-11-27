9c25048859707551cbf881ae7492cae784501301
Rasikashivhare <rasikashivhare123@gmail.com>
2021-11-27 09:39:27 +0530

    26nov

--- Java_Programs/javaexamples folder/Simple.java
+++ Java_Programs/javaexamples folder/Simple.java
@@ -1,15 +1,6 @@
-
-import java.sql.*;
 public class simple {
-    public static void main(String[] arg){
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection
-                    con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mkpits123","root","");
-                    System.out.println("successfully connected");
-        }catch(Exception ee){
-            System.out.println(ee.toString());
-        }
+    public static void main(String[] arg)
+    {
+        System.out.println("hello");
     }
 }
--- Java_Programs/javaexamples folder/Test.java
+++ Java_Programs/javaexamples folder/Test.java
@@ -1,13 +1,17 @@
-E:\javaexamples\ProductMethod.java:37: error: cannot find symbol
-p1.insertProduct(11,"monitor",12000);
-  ^
-  symbol:   method insertProduct(int,String,int)
-  location: variable p1 of type Product
-E:\javaexamples\ProductMethod.java:38: error: cannot find symbol
-p1.displayProduct();
-  ^
-  symbol:   method displayProduct()
-  location: variable p1 of type Product
-2 errors
-
-Tool completed with exit code 1
+import java.awt.*;
+public class test extends Frame{
+    Button b;
+    test();
+    {
+        b=new Button (label: 'hello');
+        b.setBounds(x: 100,y: 100,width:100,height:20);
+        add(b);
+        setLayout(null);
+        setVisible(true);
+        setSize(width:400,height: 400);
+    }
+    public static void main(String[] arg)
+    {
+        test t=new test();
+    }
+}
