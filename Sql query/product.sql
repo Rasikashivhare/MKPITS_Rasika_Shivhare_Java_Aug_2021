50a38c5d00a20c2532b4cc05f8e61166c0a2aa5b
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-08 15:34:39 +0530

    8dec

---
+++ Sql query/product.sql
@@ -0,0 +1,13 @@
+sql query to create a table product
+create table product (pid numeric,pname varchar(20),salary numeric)
+
+CREATE TABLE `mkpits1`.`product` ( `pid` INT NOT NULL AUTO_INCREMENT , `pname`
+VARCHAR(20) NOT NULL , `salary` INT NOT NULL , PRIMARY KEY (`empno`)) ;
+
+sql query to insert record in student table
+insert into student (rno,name,address) values(1,'amit','sadar');
+insert into student (name,address) values('amitabh','mumbai');
+
+sql query to view all records
+select*from student
+
