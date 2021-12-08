70942d96dfa844057afb9f69fa4e2abbc977080a
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-08 15:42:13 +0530

    8dec

---
+++ Sql query/customer.sql
@@ -0,0 +1,13 @@
+sql query to create a table customer
+create table customer (cust_id numeric,cust_name varchar(20),cust_address varchar(20)
+
+CREATE TABLE `mkpits1`.`customer` ( `cust_id` INT NOT NULL AUTO_INCREMENT , `cust_name`
+VARCHAR(20) NOT NULL , `cust_address` VARCHAR(20) , PRIMARY KEY (`cust_id`)) ;
+
+sql query to insert record in student table
+insert into student (rno,name,address) values(1,'amit','sadar');
+insert into student (name,address) values('amitabh','mumbai');
+
+sql query to view all records
+select*from student
+
