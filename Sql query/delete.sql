66cbf66796fa113cf79da3dfb1052335a5263c03
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-09 22:29:52 +0530

    9dec

---
+++ Sql query/delete.sql
@@ -0,0 +1,7 @@
+
+create a mysql delete query
+
+If we want to delete an employee whose emp_id is 107, we should use the DELETE statement with the
+WHERE clause.
+
+mysql> DELETE FROM Employees WHERE emp_id=107;
