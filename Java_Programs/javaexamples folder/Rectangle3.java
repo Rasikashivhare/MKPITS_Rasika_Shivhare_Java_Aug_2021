dffbda25d08e8ce6f9c3bc2df6da299458d6eda8
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-14 10:25:40 +0530

    14dec

---
+++ Java_Programs/javaexamples folder/Rectangle3.java
@@ -0,0 +1,27 @@
+//example of rectangle
+class Rectangle
+{
+int length;
+int width;
+void insert(int l,int w)
+{
+length=l;
+width=w;
+
+}
+void calculateArea(){System.out.println(length*width);}
+}
+public class Rectangle3
+{
+public static void main(String[] args)
+{
+Rectangle r1=new Rectangle();
+Rectangle r2=new Rectangle();
+r1.insert(4,5);
+r2.insert(7,8);
+r1.calculateArea();
+r2.calculateArea();
+
+}
+}
+
---
+++ Java_Programs/javaexamples folder/TestEmployee2.java
@@ -0,0 +1,30 @@
+//example of employee
+class Employee
+{
+int id;
+String name;
+float salary;
+void insert(int i,String n,float s)
+{
+id=i;
+name=n;
+salary=s;
+}
+void display(){System.out.println(id+""+name+""+salary);}
+}
+public class TestEmployee2
+{
+public static void main(String[] args)
+{
+Employee e1=new Employee();
+Employee e2=new Employee();
+Employee e3=new Employee();
+e1.insert(101,"rasi",50000);
+e2.insert(102,"akki",90000);
+e3.insert(103,"shubhm",4000);
+e1.display();
+e2.display();
+e3.display();
+}
+}
+
