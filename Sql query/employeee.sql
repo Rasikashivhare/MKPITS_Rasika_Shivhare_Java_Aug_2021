21d7a8d09d3ff014168ab29dd3387dfab1e517f2
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-15 21:29:07 +0530

    15dec

---
+++ Sql query/employeee.sql
@@ -0,0 +1,6 @@
+write a query to create a table employee
+
+create table employeee(id int,name varchar(20))
+
+insert into employeee(id) values
+the above command will insert null value in name column.
