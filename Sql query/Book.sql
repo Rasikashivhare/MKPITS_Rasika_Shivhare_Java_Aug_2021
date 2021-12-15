30b8b2f69a26b46f037b12ecd3a8ba6bb8eb0437
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-15 22:07:49 +0530

    15dec

---
+++ Sql query/Book.sql
@@ -0,0 +1,4 @@
+sql query to create a database
+create database mkpits123
+write a sql query to create a table book having columns bookid,title and price
+create table book (bookid numeric,title varchar(20),price numeric)
