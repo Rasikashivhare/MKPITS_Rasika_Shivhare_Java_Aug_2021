020f2510a9f9b62c6640c88b371732e61de29fde
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-15 21:35:21 +0530

    15dec

---
+++ Sql query/emp3.sql
@@ -0,0 +1,6 @@
+create a table emp3
+create table emp3(id int not null auto_increment,name varchar(20),primary key(id))
+
+insert into emp3 (name ) values('amit')
+
+insert into emp3 (name ) values('amitabh')
