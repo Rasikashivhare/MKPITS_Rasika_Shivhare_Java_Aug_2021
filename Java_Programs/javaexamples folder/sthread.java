f8a13a4f0640dab8258a841070b1ea6342c88e02
Rasikashivhare <rasikashivhare123@gmail.com>
2021-12-27 23:07:29 +0530

    27dec

---
+++ Java_Programs/javaexamples folder/sthread.java
@@ -0,0 +1,26 @@
+//single threads
+
+class th1 extends Thread
+{
+public void run()
+{
+for(int i=0;i<=10;i++)
+{
+System.out.println("thread"+getName());
+}
+}
+}
+
+
+ class singlethread
+ {
+ public static void main(String args[])
+ {
+ th1 ob1=new th1();
+ ob1.start();
+
+ Thread ov=Thread.currentThread();
+ System.out.println(ob1);
+ System.out.println(ov);
+ }
+ }
