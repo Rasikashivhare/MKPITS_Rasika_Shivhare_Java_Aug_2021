10aa043ba878147612868929dc4a241e19020735
Rasikashivhare <rasikashivhare123@gmail.com>
2022-02-01 16:57:52 +0530

    1 feb

--- Java_Programs/javaexamples folder/pred6.java
+++ Java_Programs/javaexamples folder/pred6.java
@@ -9,6 +9,7 @@ public static void main(String[] args)
 //lamda exp
 Predicate <Integer>predicate1=ar -> ar>5;
 Predicate <Integer> negate =predicate1.negate();
+
 boolean test =negate.test(3);
 System.out.println(test);
 }
